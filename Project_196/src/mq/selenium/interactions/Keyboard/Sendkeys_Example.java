62f67994938d73b0b7e1c78ea110b74335352117
sunilreddyg <sunilreddy.gajjala@gmail.com>
2021-11-22 19:21:50 +0530

    Add files via upload

---
+++ Project_196/src/mq/selenium/interactions/Keyboard/Sendkeys_Example.java
@@ -0,0 +1,32 @@
+package mq.selenium.interactions.Keyboard;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Sendkeys_Example {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\MINDQ\\Desktop\\new_drivers\\chromedriver.exe");    
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.skyscanner.co.in/");
+		driver.manage().window().maximize();
+		
+		
+		WebElement Origin_Textbox=driver.findElement(By.xpath("//input[@id='fsc-origin-search']"));
+		
+		new Actions(driver)
+		.sendKeys(Origin_Textbox, Keys.BACK_SPACE)
+		.pause(1000).sendKeys("DEL")
+		.pause(2000).sendKeys(Keys.ARROW_DOWN)
+		.pause(2000).sendKeys(Keys.ENTER).perform();
+		
+		
+
+	}
+
+}
--- Project_196/src/mq/selenium/interactions/Mouse/Drag_And_DropBy_Target.java
+++ Project_196/src/mq/selenium/interactions/Mouse/Drag_And_DropBy_Target.java
@@ -1,17 +1,16 @@
 package mq.selenium.interactions.Mouse;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class Drag_And_DropBy_Target {
+public class Drag_And_DropBy_Target 
+{
 
 	public static void main(String[] args) throws Exception {
-		
 		
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\MINDQ\\Desktop\\new_drivers\\chromedriver.exe");    
 		WebDriver driver=new ChromeDriver();
@@ -20,8 +19,6 @@ public class Drag_And_DropBy_Target {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		Thread.sleep(4000);
 		
-		
-
 		WebElement ScrollButton=driver.findElement(By.xpath("(//div[@class='rangeslider__handle'])[1]"));
 		new Actions(driver).dragAndDropBy(ScrollButton, 150, 0).perform();
 		
